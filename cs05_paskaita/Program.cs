53982c13cd44adbcdb11232501c2d16b44715ab4
Surkus <v.surkus@me.com>
2022-01-05 18:09:49 +0200

    Initial commit

---
+++ cs05_paskaita/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cs05_paskaita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
